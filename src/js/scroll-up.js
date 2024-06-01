5a235ee603475221df9cc23dffd7df73de5f785f
Vikto0004 <vikto0004@gmail.com>
2024-06-01 21:42:41 +0200

    add scroll up

---
+++ src/js/scroll-up.js
@@ -0,0 +1,27 @@
+const elScrollBtn = document.querySelector('.scroll-btn ');
+
+let lastScrollTop = 0;
+let switchScroll = true;
+
+window.addEventListener('scroll', function () {
+  let currentScrollTop = window.pageYOffset;
+
+  if (currentScrollTop < lastScrollTop && switchScroll) {
+    elScrollBtn.classList.add('is-active-scroll');
+    switchScroll = false;
+  } else if (currentScrollTop > lastScrollTop && !switchScroll) {
+    elScrollBtn.classList.remove('is-active-scroll');
+    switchScroll = true;
+  } else if (currentScrollTop === 0) {
+    elScrollBtn.classList.remove('is-active-scroll');
+  }
+  lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+});
+
+elScrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+  elScrollBtn.classList.remove('is-active-scroll');
+});
--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ import './js/render-projects.js';
 import './js/reviews.js';
 import './js/form.js';
 import './js/accordion.js';
+import './js/scroll-up.js';
