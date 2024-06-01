d6623ecc8692aa128127c9ba14642982502b2648
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-06-01 14:51:54 +0300

    border fix, upd accordion.js, udp faq.html
    refactoring faq.html for correct Accordion work;
    replacement static border-bottom in list-items to pseudo elements;
    add options in accordion.js

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -3,4 +3,33 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion-container');
+const getPreviousElement = currElement => {
+  let valueId = currElement.id;
+  let valueIdToNumber = Math.abs(parseInt(valueId.replace(/[^0-9]/g, '')));
+  const previousElem = document.getElementById(`ac-${valueIdToNumber - 1}`);
+  return previousElem;
+};
+
+new Accordion('.accordion-container', {
+  openOnInit: [0],
+  beforeOpen: currElement => {
+    const previousElem = getPreviousElement(currElement);
+
+    if (currElement.id !== 'ac-0') {
+      previousElem.classList.add('border-animate');
+      currElement.classList.add('border-animate');
+    } else {
+      currElement.classList.add('border-animate');
+    }
+  },
+  beforeClose: currElement => {
+    const previousElem = getPreviousElement(currElement);
+
+    if (currElement.id !== 'ac-0') {
+      previousElem.classList.remove('border-animate');
+      currElement.classList.remove('border-animate');
+    } else {
+      currElement.classList.remove('border-animate');
+    }
+  },
+});
