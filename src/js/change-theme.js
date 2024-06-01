2949dfa83e1d4dc6461926ec08f7204d06caf8df
Vikto0004 <vikto0004@gmail.com>
2024-06-01 16:42:05 +0200

    add dark-favicon theme

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -1,5 +1,9 @@
+import lightFavicon from '/public/light-favicon.svg';
+import darkFavicon from '/public/dark-favicon.svg';
+
 const checkboxEl = document.querySelector('.theme-checkbox');
 const bodyEl = document.querySelector('body');
+const faviconEl = document.querySelector('link[rel="icon"]');
 
 let isDarkThemeActive = false;
 const savedTheme = localStorage.getItem('dark-theme');
@@ -7,14 +11,15 @@ const savedTheme = localStorage.getItem('dark-theme');
 if (savedTheme == 'true') {
   isDarkThemeActive = true;
   document.body.classList.add('dark-theme');
-}
-
-if (isDarkThemeActive) {
+  faviconEl.href = darkFavicon;
   checkboxEl.checked = true;
 }
 
 checkboxEl.addEventListener('change', () => {
   bodyEl.classList.toggle('dark-theme');
+  if (isDarkThemeActive) faviconEl.href = lightFavicon;
+  else faviconEl.href = darkFavicon;
+
   isDarkThemeActive = !isDarkThemeActive;
   localStorage.setItem('dark-theme', isDarkThemeActive);
 });
